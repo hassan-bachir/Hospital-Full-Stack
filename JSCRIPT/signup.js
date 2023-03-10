c36de24f16b69eb98b30d6ae0c479bd11cab8fda
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-03-10 21:56:30 +0200

    added inputs for patient and employees

--- JSCRIPT/signup.js
+++ JSCRIPT/signup.js
@@ -8,3 +8,28 @@ toggle.onclick = function () {
     const isopen =dropdown.classList.contains('open');}
     
 
+    function showForm1() {
+        var form = document.getElementById("myForm");
+        if (form.style.display === "none") {
+           form.style.display = "block";
+        } else {
+           form.style.display = "none";
+        }
+     }
+
+     const test = document.querySelector('#test');
+     test.addEventListener('click',showForm1);
+     
+     
+    function showForm2() {
+        var form = document.getElementById("myForm1");
+        if (form.style.display === "none") {
+           form.style.display = "block";
+        } else {
+           form.style.display = "none";
+        }
+     }
+
+     const test1 = document.querySelector('#test1');
+     test1.addEventListener('click',showForm2);
+     
