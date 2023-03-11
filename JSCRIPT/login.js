7b8dd522379dfe849b1505ed62a16d2998c09b71
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-03-11 15:49:22 +0200

    finished the login api

--- JSCRIPT/login.js
+++ JSCRIPT/login.js
@@ -39,36 +39,36 @@ loginButton.addEventListener('click',(event)=>{
 
 });
 
+function signin() {
+  let username = document.getElementById('login-user-name').value;
+  let password = document.getElementById('login-password').value;
+  let data = new FormData();
+  data.append('username', username);
+  data.append('password', password);
+  
+  axios.post('http://localhost/fullstack-ecommerce-project/login.php', data)
+  .then(function (res) {
+    console.log(res.data)
+    if (res.data.response === "logged in") {
+      // Save user ID in sessionStorage
+      window.sessionStorage.setItem('user_id', res.data.id);
+      
+      window.location.href = 'main.html';
+    } else if (res.data.response === "user not found") {
+      
+      alert("User not found.");
+    } else if (res.data.response === "Incorrect password") {
+     
+      alert("Incorrect password.");
+    }
+  }).catch(function (err) {
+    console.log(err);
+  });
+}
 
 
 
 
 
   
-function signin() {
-    let username = document.getElementById('login-user-name').value;
-    let password = document.getElementById('login-password').value;
-    let data = new FormData();
-    data.append('username', username);
-    data.append('password', password);
-    
-    axios.post('http://localhost/fullstack-ecommerce-project/login.php', data)
-    .then(function (res) {
-      console.log(res.data)
-      if (res.data.response === "logged in") {
-        // Save user ID in sessionStorage
-        window.sessionStorage.setItem('user_id', res.data.id);
-        
-        window.location.href = 'main.html';
-      } else if (res.data.response === "user not found") {
-        
-        alert("User not found.");
-      } else if (res.data.response === "Incorrect password") {
-       
-        alert("Incorrect password.");
-      }
-    }).catch(function (err) {
-      console.log(err);
-    });
-  }
   
