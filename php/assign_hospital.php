3588471a205b62d655bc651d8ddca2ae59a57b2f
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-03-12 16:50:13 +0200

    fixed query error

--- php/assign_hospital.php
+++ php/assign_hospital.php
@@ -4,14 +4,6 @@ include('connection.php');
 header('Allow-Control-Allow-Origin: *');
 header('Allow-Control-Allow-Methods: POST,OPTIONS');
 
-
-// $_SESSION['id'] = $id;
-// $_SESSION['username']= $username1;
-// $_SESSION['email'] = $email;
-// $_SESSION['password']=$password;
-// $_SESSION['dob']=$dob;
-// $_SESSION['usertype_id']=$usertype_id;
-
 if ($_SESSION['usertype_id']=='admin'){
    
     
