38982ee7e3b777d7efdafb7ace8b047451ba8cc0
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-03-12 14:50:54 +0200

    finished API whow users

--- php/show_patiennts.php
+++ php/show_patiennts.php
@@ -1,6 +1,17 @@
 <?php
 include('connection.php');
 
-$query = $mysqli->prepare("SELECT * FROM patients");
+$result = $mysqli->query("SELECT * FROM patients");
 
 $data =[];
+
+
+if ($result->num_rows > 0) {
+    // iterate over rows and add to data array
+    while($row = $result->fetch_assoc()) {
+      $data[] = $row;
+    }
+  }
+
+  echo json_encode($data);
+?>
